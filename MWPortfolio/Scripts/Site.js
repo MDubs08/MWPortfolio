941d31ace2efa9997d8f95de215049158c29e6ad
Mike Wilson <mdubs08@gmail.com>
2017-03-28 11:24:40 -0500

    Initial Drop
    Initial drop for MW Portfolio Website

---
+++ MWPortfolio/Scripts/Site.js
@@ -0,0 +1,9 @@
+﻿//Navbar//
+
+$(window).scroll(function () {
+    if ($(window).scrollTop() > 100) {
+        $('.main').addClass('sticky');
+    } else {
+        $('.main').removeClass('sticky');
+    }
+});
