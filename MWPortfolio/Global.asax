f3fb456118c611f53c83f4cd6db6f02ab76fefa5
Mike Wilson <mdubs08@gmail.com>
2017-03-28 11:11:48 -0500

    Add project files.

---
+++ MWPortfolio/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MWPortfolio.MvcApplication" Language="C#" %>
---
+++ MWPortfolio/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MWPortfolio.Startup))]
+namespace MWPortfolio
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
